55d435d9c7e9acc78b119edb4e552b5c386a71b8
sohun-cs <asaduzzamansohun5@gmail.com>
2025-07-03 20:37:20 +0600

    Monkey EOF

---
+++ monkey-eof.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    char S[100005];
+
+    while (cin.getline(S, 100005))
+    {
+
+        int x = strlen(S);
+
+        sort(S, (S + x));
+
+        for (int i = 0; i < x; i++)
+        {
+            if (S[i] == ' ')
+            {
+                continue;
+            }
+            cout << S[i];
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
