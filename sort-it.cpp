402d5884c4f9e12dec214a9f1fb4972a2c98c174
sohun-cs <asaduzzamansohun5@gmail.com>
2025-07-03 17:52:38 +0600

    sorting with sorting function

---
+++ sort-it.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int N;
+    cin >> N;
+
+    long long A[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    sort(A, A + N);
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << " ";
+    }
+
+    cout << endl;
+
+    sort(A, A + N, greater<int>());
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << " ";
+    }
+
+    return 0;
+}
